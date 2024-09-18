e7cdfa0d0a5c2cf13d7e3c6ab21075f36fbe6f3c
Paul <paul.turkovskiy@gmail.com>
2024-09-18 04:18:30 -0400

    add intents, sync slash commands

--- bot.py
+++ bot.py
@@ -3,14 +3,15 @@ import nextcord
 from nextcord.ext import commands
 import os
 
-intents = nextcord.Intents.default()
+intents = nextcord.Intents.all()
 bot = commands.Bot(command_prefix="%", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    await bot.load_extension("cogs.maps")
-    await bot.load_extension("cogs.admin")
+    bot.load_extension("cogs.maps")
+    bot.load_extension("cogs.admin")
+    await bot.sync_all_application_commands()
+    print("Slash commands are synced")
 
-# Запуск бота
 bot.run(os.getenv("ANILY_BOT_TOKEN"))
