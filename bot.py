ca18becd723d1470dfa28c5e03ab9dd65dacb0fe
Paul <paul.turkovskiy@gmail.com>
2024-09-22 08:33:29 -0400

    add better logging

--- bot.py
+++ bot.py
@@ -1,17 +1,28 @@
 #!/bin/env python3
 import nextcord
 from nextcord.ext import commands
+import logging
 import os
+import sys
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    stream=sys.stdout
+)
+logger = logging.getLogger(__name__)
 
 intents = nextcord.Intents.all()
 bot = commands.Bot(command_prefix="%", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    logger.info(f"Logged in as {bot.user}")
     bot.load_extension("cogs.maps")
     bot.load_extension("cogs.admin")
+    bot.load_extension("cogs.stats")
+    await bot.discover_application_commands()
     await bot.sync_all_application_commands()
-    print("Slash commands are synced")
+    logger.info(f"{len(bot.get_all_application_commands())} slash commands are synced")
 
 bot.run(os.getenv("ANILY_BOT_TOKEN"))
